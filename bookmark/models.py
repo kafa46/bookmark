88f40c196599bd9d15354fb4c7d6f80d05259832
kafa46_cju <kafa46@cju.ac.kr>
2021-02-09 04:00:28 +0900

    Bookmark Service

---
+++ bookmark/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.urls import reverse
+
+class Bookmark(models.Model):
+    site_name = models.CharField(max_length=100)
+    url = models.URLField('Site URL')
+
+    def get_absolute_url(self):
+        return reverse('detail', args=[str(self.id)])
+
+    def __str__(self):
+         return "이름: {}, 주소: {}".format(self.site_name, self.url)
